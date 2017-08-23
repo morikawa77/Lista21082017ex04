178595f35f450cd02caa342077468edd8e9c67c8
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-23 07:50:04 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lista21082017ex04
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			decimal valorReais, cotacaoDolar, valorDolares;
+
+			Console.Write("Digite o valor em Reais: ");
+			valorReais = Convert.ToDecimal(Console.ReadLine());
+			Console.Write("Digite a cotação do dólar: ");
+			cotacaoDolar = Convert.ToDecimal(Console.ReadLine());
+
+			valorDolares = valorReais * cotacaoDolar;
+            Console.Write("O valor em dólares é US$ {0:##,##0.00}", valorDolares);
+			
+			Console.ReadLine();
+        }
+    }
+}
